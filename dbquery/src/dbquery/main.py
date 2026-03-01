b58455ae08c89fbc43e50ed611ff2fd9c2939b73
UzairUlhaq <ulhaquzair@gmail.com>
2026-03-01 06:25:56 +0100

    ready

--- dbquery/src/dbquery/main.py
+++ dbquery/src/dbquery/main.py
@@ -3,7 +3,7 @@ import sys
 from dbquery.crew import DbQueryCrew
 
 def run():
-    inputs = {'user_question': 'Find 10 users from Padova?'}
+    inputs = {'user_question': 'Find people with age greater than 30?'}
     result = DbQueryCrew().crew().kickoff(inputs=inputs)
     
 if __name__ == "__main__":
